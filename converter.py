1f0d77e911481eaea37b88a2ceb76268e03739ca
Anna-Mazina <galimova.pr@gmail.com>
2020-07-14 12:28:32 +0300

    Добавлена обработка полученного *.md файла удаление ненужных строк

--- converter.py
+++ converter.py
@@ -3,18 +3,31 @@ import os
 import glob
 import pypandoc
 import shutil
+import codecs
 
 if not os.path.isdir("Обработано"):
      os.mkdir("Обработано")
 
-new_name = 'a'
-for infile in glob.glob("*.html"):
-     doc = pypandoc.convert_file(infile, 'md', outputfile= new_name + ".md")
-     new_name += '1'
 
-for a in glob.glob('*.md'):
-    new_name = '{}_{}'.format(os.path.basename(os.path.dirname(a)), os.path.basename(a))
-    shutil.move(a, os.path.join('Обработано', new_name))
+for infile in glob.glob("*.html"):
+    new_name = '{}_{}'.format(os.path.basename(os.path.dirname(infile)), os.path.basename(infile))
+    print('Файл', infile, 'обрабатывается')
+    doc = pypandoc.convert_file(infile, 'md', outputfile= new_name + ".md")
+
+    for infile in glob.glob("*.md"):
+        with open(infile, "r") as file:
+             lines = file.readlines()
+
+        print('Файл', infile, 'редактируется')
+
+        with open(infile, 'w') as f:
+             for line in lines:
+                 if ":::" not in line:
+                     f.write(line)
+
+        for a in glob.glob('*.md'):
+            new_name = '{}_{}'.format(os.path.basename(os.path.dirname(a)), os.path.basename(a))
+            shutil.move(a, os.path.join('Обработано', new_name))
 
 
 print('Готово')
