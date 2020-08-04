074234b64ace8b9a6dc33f40e7ecab10b63dfdbd
Anna-Mazina <galimova.pr@gmail.com>
2020-08-04 10:52:18 +0300

    Добавлены комментарии

--- converter.py
+++ converter.py
@@ -5,15 +5,17 @@ import pypandoc
 import shutil
 import codecs
 
+# создаем папку, в которую будут перенесены обработнные файлы
 if not os.path.isdir("Обработано"):
      os.mkdir("Обработано")
 
-
+# Открываем файлы формата *.html, имеющиеся в текущей папке. Конвертируем его в *.md
 for infile in glob.glob("*.html"):
-    new_name = '{}_{}'.format(os.path.basename(os.path.dirname(infile)), os.path.basename(infile))
+    new_name = '{}_{}'.format(os.path.basename(os.path.dirname(infile)), os.path.basename(infile))  # Присваиваем новове имя файлу
     print('Файл', infile, 'обрабатывается')
-    doc = pypandoc.convert_file(infile, 'md', outputfile= new_name + ".md")
+    doc = pypandoc.convert_file(infile, 'md', outputfile= new_name + ".md")   # Конвертируем файл в формат *.md
 
+# После конвертации убираем артефакты, не нужные строки
     for infile in glob.glob("*.md"):
         with open(infile, "r") as file:
              lines = file.readlines()
@@ -24,6 +26,10 @@ for infile in glob.glob("*.html"):
              for line in lines:
                  if ":::" not in line:
                      f.write(line)
+
+        print('Файл', infile, 'перенесен в папку "Обработано"')
+
+        # После всех обработок файл переносим в папку "Обработано"
 
         for a in glob.glob('*.md'):
             new_name = '{}_{}'.format(os.path.basename(os.path.dirname(a)), os.path.basename(a))
